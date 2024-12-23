37cd515c1014aedf92c5dfb4d9b1bedb97028f69
Subha Ghanta <subhaghanta60@gmail.com>
2024-12-23 11:39:34 +0530

    Generic Views | ListAPIView & RetrieveAPIView

--- DjangoRestFramework/Api/views.py
+++ DjangoRestFramework/Api/views.py
@@ -7,24 +7,48 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.db.models import Max
 
+from rest_framework import generics
 
-@api_view(['GET'])
-def product_list(request):
-    products=Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
+
+
+
+class ProductListAPIView(generics.ListAPIView):
+
+    queryset = Product.objects.filter(stock__gt=0)
+    serializer_class = ProductSerializer
+   
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
+
+
+
+class OrderListAPIView(generics.ListAPIView):
+
+    queryset = Order.objects.prefetch_related('items','items__product').all()
+    serializer_class = OrderSerializer
+
+
+
+# @api_view(['GET'])
+# def product_list(request):
+#     products=Product.objects.all()
+#     serializer = ProductSerializer(products, many=True)
 
     # return JsonResponse({
     #     'data': serializer.data
     # })
-    return Response(serializer.data)
+    # return Response(serializer.data)
 
 
-@api_view(['GET'])
-def product_details(request,pk):
+# @api_view(['GET'])
+# def product_details(request,pk):
 
-    product=get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+#     product=get_object_or_404(Product, pk=pk)
+#     serializer = ProductSerializer(product)
+#     return Response(serializer.data)
 
 
 
@@ -38,13 +62,13 @@ def product_details(request,pk):
 
 #optimize Queries
 
-@api_view(['GET'])
-def order_list(request):
-    #order=Order.objects.prefetch_related('items','items__product').all()
-    order=Order.objects.prefetch_related('items__product').all()
-    serializer = OrderSerializer(order, many=True)
+# @api_view(['GET'])
+# def order_list(request):
+#     #order=Order.objects.prefetch_related('items','items__product').all()
+#     order=Order.objects.prefetch_related('items__product').all()
+#     serializer = OrderSerializer(order, many=True)
 
-    return Response(serializer.data)
+#     return Response(serializer.data)
 
 
 @api_view(['GET'])
--- DjangoRestFramework/DjangoRestFramework/urls.py
+++ DjangoRestFramework/DjangoRestFramework/urls.py
@@ -19,9 +19,14 @@ from  Api import views
 
 urlpatterns = [
     
-    path('products/', views.product_list),
+    # path('products/', views.product_list),
+    # path('products/<int:pk>/', views.product_details),
+    #  path('orders/', views.order_list),
+    path('products/', views.ProductListAPIView.as_view()),
+    #  path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('products/<int:product_id>/' , views.ProductDetailAPIView.as_view()),
      path('products-info/', views.product_info),
-     path('products/<int:pk>/', views.product_details),
-     path('orders/', views.order_list),
+     path('orders/', views.OrderListAPIView.as_view()),
+    
      path('silk/', include('silk.urls',namespace='silk'))
 ]
