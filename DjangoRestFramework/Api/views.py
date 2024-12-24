b01d191f6b17f07df99d72126edb83487a973fd5
Subha Ghanta <subhaghanta60@gmail.com>
2024-12-24 16:37:24 +0530

    [Rest Framwork] SearchFilter and Ordering Filter

--- DjangoRestFramework/Api/views.py
+++ DjangoRestFramework/Api/views.py
@@ -20,6 +20,9 @@ from rest_framework.views import APIView
 
 from Api.filters import ProductFilter
 
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class ProductListAPIView(generics.ListAPIView):
 
@@ -67,6 +70,13 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     # filterset_fields = ('name','price')
     filterset_class= ProductFilter
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+        ]
+    search_fields = ['=name','description']
+    ordering_fields =['name','price','stock']
 
     def get_permissions(self):
         self.permission_classes = [AllowAny]
