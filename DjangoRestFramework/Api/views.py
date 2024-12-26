3567618524f5418aefc3273280d62bad964616ff
Subha Ghanta <subhaghanta60@gmail.com>
2024-12-26 09:57:43 +0530

    [Rest Framework] Pagination

--- DjangoRestFramework/Api/views.py
+++ DjangoRestFramework/Api/views.py
@@ -23,6 +23,8 @@ from Api.filters import ProductFilter, InstockFilterBackend
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+
 
 class ProductListAPIView(generics.ListAPIView):
 
@@ -66,7 +68,8 @@ class ProductCreateAPIView(generics.CreateAPIView):
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
+    queryset = Product.objects.order_by('pk').all()
     serializer_class = ProductSerializer
     # filterset_fields = ('name','price')
     filterset_class= ProductFilter
@@ -78,6 +81,12 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         ]
     search_fields = ['=name','description']
     ordering_fields =['name','price','stock']
+    # pagination_class = PageNumberPagination
+    # pagination_class.page_size=2
+    # pagination_class.page_query_param = 'pagenum'
+    # pagination_class.page_size_query_param = 'size'
+    # pagination_class.max_page_size = 6
+    pagination_class = LimitOffsetPagination
 
     def get_permissions(self):
         self.permission_classes = [AllowAny]
